89cc0e7769db39c683126e23662b33f518ae6b23
asuchovarov <oblomingo@gmail.com>
2016-08-19 08:27:15 +0300

    Package service added

---
+++ Stock.Packages/Domain/Package.cs
@@ -0,0 +1,38 @@
+﻿using Stock.Messages.Dto;
+
+namespace Stock.Packages.Domain
+{
+    public class Package
+    {
+        private int _orderId;
+        public Package(int packageId, int orderId)
+        {
+            PackageId = packageId;
+            _orderId = orderId;
+        }
+
+        private double _height;
+        private double _width;
+        private double _depth;
+        private double _weigth;
+
+        public int PackageId { get; }
+
+        public void SetPackageSize(double height, double width, double depth)
+        {
+            _height = height;
+            _width = width;
+            _depth = depth;
+        }
+
+        public void SetPackageWeigth(double weigth)
+        {
+            _weigth = weigth;
+        }
+
+        public string SizeAndWeigth => 
+            $"heigth: {_height}, width: {_width}, depth: {_depth}, weigth: {_weigth}";
+
+        public PackageSize Size => new PackageSize {Height = _height, Width = _width, Depth = _depth, Weigth = _weigth};
+    }
+}
